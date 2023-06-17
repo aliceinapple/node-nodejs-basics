bef7e8a4757110f853f96476d9548051ab625f69
Viktoria <aliceinapple99@gmail.com>
2023-06-17 15:11:10 +0300

    feat: implement streams/write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import fs from "fs";
+import path from "path";
+
+const __filename = process.argv[1];
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, "files", "fileToWrite.txt");
+
+  const writableStream = fs.createWriteStream(filePath);
+
+  process.stdin.pipe(writableStream);
 };
 
 await write();
