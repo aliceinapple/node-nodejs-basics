9daec2fac94c3902b91dc858718bafd5c0d9a646
Viktoria <aliceinapple99@gmail.com>
2023-06-17 17:09:40 +0300

    feat: implement zip/decompress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -6,13 +6,13 @@ import { pipeline } from "stream/promises";
 const __filename = process.argv[1];
 const __dirname = path.dirname(__filename);
 
-const compress = async () => {
-  const inputFilePath = path.join(__dirname, "files", "fileToCompress.txt");
-  const outputFilePath = path.join(__dirname, "files", "archive.gz");
+const inputFilePath = path.join(__dirname, "files", "fileToCompress.txt");
+const outputFilePath = path.join(__dirname, "files", "archive.gz");
 
+const compress = async () => {
   const readStream = fs.createReadStream(inputFilePath);
-  const writeStream = fs.createWriteStream(outputFilePath);
   const gzipStream = zlib.createGzip();
+  const writeStream = fs.createWriteStream(outputFilePath);
 
   await pipeline(readStream, gzipStream, writeStream);
 };
