047f4e9e80bb7bc5cc53de9e6318e17d87dd1328
Viktoria <aliceinapple99@gmail.com>
2023-06-17 11:15:43 +0300

    feat: implement create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import fs from "fs";
+import path from "path";
+
+const __filename = process.argv[1];
+const __dirname = path.dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, "files", "fresh.txt");
+
+  try {
+    await fs.promises.access(filePath, fs.constants.F_OK);
+    throw new Error("FS operation failed: File already exists");
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      await fs.promises.writeFile(filePath, "I am fresh and young");
+    } else {
+      throw error;
+    }
+  }
 };
 
-await create();
+create().catch((error) => {
+  console.error(error.message);
+});
