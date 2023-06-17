9dd683c9e2d1afc4e7d70635795057295912b8df
Viktoria <aliceinapple99@gmail.com>
2023-06-17 12:46:44 +0300

    feat: implement copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,44 @@
+import fs from "fs/promises";
+import path from "path";
+
+const __filename = process.argv[1];
+const __dirname = path.dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+  const sourceFolderPath = path.join(__dirname, "files");
+  const folderPath = path.join(__dirname, "files_copy");
+
+  try {
+    const sourceFolderExist = await fs
+      .access(sourceFolderPath, fs.constants.F_OK)
+      .then(() => true)
+      .catch(() => false);
+
+    if (!sourceFolderExist) {
+      throw new Error("FS operation failed: Folder 'files' doesn't exist");
+    }
+
+    const folderExists = await fs
+      .access(folderPath, fs.constants.F_OK)
+      .then(() => true)
+      .catch(() => false);
+
+    if (!folderExists) {
+      fs.mkdir(folderPath);
+
+      fs.readdir(sourceFolderPath).then((files) => {
+        files.forEach((file) => {
+          const oldPath = path.join(sourceFolderPath, file);
+          const newPath = path.join(folderPath, file);
+          fs.copyFile(oldPath, newPath);
+        });
+      });
+    } else {
+      throw new Error("FS operation failed: Target folder already exists");
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,4 +1,4 @@
-import fs from "fs";
+import fs from "fs/promises";
 import path from "path";
 
 const __filename = process.argv[1];
@@ -8,17 +8,19 @@ const create = async () => {
   const filePath = path.join(__dirname, "files", "fresh.txt");
 
   try {
-    await fs.promises.access(filePath, fs.constants.F_OK);
-    throw new Error("FS operation failed: File already exists");
-  } catch (error) {
-    if (error.code === "ENOENT") {
-      await fs.promises.writeFile(filePath, "I am fresh and young");
+    const fileExists = await fs
+      .access(filePath, fs.constants.F_OK)
+      .then(() => true)
+      .catch(() => false);
+
+    if (!fileExists) {
+      await fs.writeFile(filePath, "I am fresh and young");
     } else {
-      throw error;
+      throw new Error("FS operation failed: File already exists");
     }
+  } catch (error) {
+    console.error(error.message);
   }
 };
 
-create().catch((error) => {
-  console.error(error.message);
-});
+await create();
