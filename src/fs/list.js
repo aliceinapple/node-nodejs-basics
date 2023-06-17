24ded3392036f709cb0fee48389456f78e1b4219
Viktoria <aliceinapple99@gmail.com>
2023-06-17 13:22:35 +0300

    feat: implement list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,27 @@
+import fs from "fs/promises";
+import path from "path";
+
+const __filename = process.argv[1];
+const __dirname = path.dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+  const folderPath = path.join(__dirname, "files");
+
+  try {
+    const folderExists = await fs
+      .access(folderPath, fs.constants.F_OK)
+      .then(() => true)
+      .catch(() => false);
+
+    if (!folderExists) {
+      throw new Error("FS operation failed: folder doesn't exist");
+    }
+
+    const folderContent = await fs.readdir(folderPath);
+    console.log(folderContent);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 await list();
