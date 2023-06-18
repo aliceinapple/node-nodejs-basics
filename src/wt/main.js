2a3175c6ab656ce416814c46544b7e116b28cdd5
Viktoria <aliceinapple99@gmail.com>
2023-06-18 17:33:26 +0300

    feat: implement worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,43 @@
+import { cpus } from "os";
+import { Worker } from "worker_threads";
+import path from "path";
+
+const __filename = process.argv[1];
+const __dirname = path.dirname(__filename);
+
+const workerPath = path.join(__dirname, "worker.js");
+
 const performCalculations = async () => {
-    // Write your code here
+  const numberOfCores = cpus().length;
+
+  const createWorkerPromise = (index) => {
+    const initialValue = 10;
+    const worker = new Worker(workerPath, {
+      workerData: index + initialValue,
+    });
+
+    return new Promise((resolve, reject) => {
+      worker.on("message", resolve);
+      worker.on("error", reject);
+    });
+  };
+
+  const processWorkerPromises = (settledPromises) => {
+    return settledPromises.map((promiseResult) => {
+      return {
+        status: promiseResult.status === "fulfilled" ? "resolved" : "error",
+        data: promiseResult.status === "fulfilled" ? promiseResult.value : null,
+      };
+    });
+  };
+
+  const workerPromises = Array.from({ length: numberOfCores }, (_, i) =>
+    createWorkerPromise(i)
+  );
+  const settledPromises = await Promise.allSettled(workerPromises);
+  const calculationLog = processWorkerPromises(settledPromises);
+
+  console.log(calculationLog);
 };
 
 await performCalculations();
